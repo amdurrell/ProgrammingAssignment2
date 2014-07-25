c9903a40ebce2b5f23bacf707933b1b7993c99aa
amdurrell <adurrell@gmail.com>
2014-07-25 15:11:14 -0700

    Wrote functions for programming assignment 2
    makeCacheMatrix
    cacheSolve

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,24 @@
-## Put comments here that give an overall description of what your
-## functions do
+##The following functions cache the inverse of a matrix and check the cache.
 
-## Write a short comment describing this function
-
+##Caches a matrix containing the given matrix and its inverse.
 makeCacheMatrix <- function(x = matrix()) {
-
+  #Find inverse
+  myInverse <- solve(x)
+  #Cache it with x in a matrix
+  myCached <<- matrix(cbind(x, myInverse))
+  return(myCached)
+  
 }
 
 
-## Write a short comment describing this function
-
+## Checks cache before calculating the inverse of matrix x
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  #Check whether matrix x is identical to cached matrix
+  if (x == myCached[1]) {
+    myInverse <- myCached[2]}
+  #If matrix x is not identical to cached matrix, calculate the inverse
+  else {
+    myInverse <- solve(x)
+  }
+  return(myInverse)
 }
